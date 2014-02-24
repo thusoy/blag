08f2db1ca737b4e5e3e09153b6cfc24dac856a67
Tarjei Husøy <tarjei@roms.no>
2014-02-24 21:43:36 +0100

    Debug s3 push

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,2 +1,4 @@
-aws s3 cp dist/static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz
-aws s3 cp dist/thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz
+ll ~/.aws
+cat ~/.aws/config
+aws s3 cp dist/static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz --debug
+aws s3 cp dist/thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz --debug
