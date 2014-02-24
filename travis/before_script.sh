b450b389801965a048175b225d5efc4c60e9df92
Tarjei Husøy <tarjei@roms.no>
2014-02-24 20:20:07 +0100

    Push builds to s3

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -4,3 +4,12 @@
 git remote set-url origin $REPO.git
 git config --global user.email "tarjei@roms.no"
 git config --global user.name "Tarjei Husøy (via Travis CI)"
+
+# Install aws cli and configure
+pip install awscli
+echo > ~/.aws/config <<aws-iam-config
+[default]
+aws_access_key_id = $S3_ACCESS_ID
+aws_secret_access_key = $S3_ACCESS_KEY
+region = eu-west-1
+aws-iam-config
