cd9b2f28ce7d8e6bf98a46b0544e2a0a0d330405
Tarjei Husøy <tarjei@roms.no>
2014-04-09 17:52:48 +0200

    Don't leak access credentials on travis

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,3 @@
-ll ~/.aws
-cat ~/.aws/config
-aws s3 cp dist/static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz --debug
-aws s3 cp dist/thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz --debug
+cd dist
+aws s3 cp static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz --debug
+aws s3 cp thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz --debug
