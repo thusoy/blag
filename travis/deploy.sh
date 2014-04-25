7faa20bde37a7bfdbbdc4f55196e72af6c317dff
Tarjei Husøy <tarjei@roms.no>
2014-04-25 14:31:36 +0200

    Remove debug flag from deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,3 +1,2 @@
-cd dist
-aws s3 cp static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz --acl public-read --debug
-aws s3 cp thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz --acl public-read --debug
+aws s3 cp dist/static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz --acl public-read
+aws s3 cp dist/thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz --acl public-read
