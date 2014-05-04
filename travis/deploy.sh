543007ffe4c140317f77a3c9b0f0eab61abe60d7
Tarjei Husøy <tarjei@roms.no>
2014-05-04 11:46:12 +0200

    Deploy sha1 file hashes to s3

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,2 +1,10 @@
-aws s3 cp dist/static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz --acl public-read
-aws s3 cp dist/thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz --acl public-read
+# Compute the file hashes
+sha1sum dist/static.tar.gz | cut -f 1 -d " " > dist/static.tar.gz.sha1
+sha1sum dist/thusoy-blag-0.1.0.tar.gz | cut -f 1 -d " " > dist/thusoy-blag-0.1.0.tar.gz.sha1
+
+# Upload hashes and files
+aws s3 cp --acl public-read dist/thusoy-blag-0.1.0.tar.gz s3://thusoy.com/dist/blag.tar.gz
+aws s3 cp --acl public-read dist/thusoy-blag-0.1.0.tar.gz.sha1 s3://thusoy.com/dist/blag.tar.gz.sha1
+
+aws s3 cp --acl public-read dist/static.tar.gz s3://thusoy.com/dist/blag-static.tar.gz
+aws s3 cp --acl public-read dist/static.tar.gz.sha1 s3://thusoy.com/dist/blag-static.tar.gz.sha1
