bcae28cf42c472e33207771ba77d3b2ecb51102a
Tarjei Husøy <tarjei@roms.no>
2014-05-04 13:54:19 +0200

    Revert "Push builds to s3"
    This reverts commit b450b389801965a048175b225d5efc4c60e9df92.
    
    Using S3 for this was just a mess of permissions, flaky cli tools and
    general confusion. GitHub will have to do for now, when I have my own
    fileserver running I might swap to that.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -4,13 +4,3 @@
 git remote set-url origin $REPO.git
 git config --global user.email "tarjei@roms.no"
 git config --global user.name "Tarjei Husøy (via Travis CI)"
-
-# Install aws cli and configure
-pip install awscli
-
-mkdir ~/.aws
-echo "[default]
-aws_access_key_id = $TRAVIS_IAM_ACCESS_ID
-aws_secret_access_key = $TRAVIS_IAM_ACCESS_KEY
-region = eu-west-1
-" > ~/.aws/config
