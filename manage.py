8cafe47723d169e05fc5d9572f2da06132a0d800
Tarjei Husøy <tarjei@roms.no>
2014-05-15 01:00:38 +0200

    Set default config file in manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,8 @@
-from thusoy.scripts import manager
+import os
+
+os.environ.setdefault('BLAG_CONFIG_FILE', os.path.abspath(os.path.join(os.path.dirname(__file__), 'dev_settings.py')))
+
+from blag.scripts import manager
 
 if __name__ == '__main__':
     manager.run()
