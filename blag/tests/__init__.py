abc983927c1d55dd4a25f612a2a21aa8dccfca1c
Tarjei Husøy <tarjei@roms.no>
2014-05-15 15:51:30 +0200

    Refactor tests

---
+++ blag/tests/__init__.py
@@ -0,0 +1,86 @@
+from .. import create_app, db
+from ..auth.models import User
+
+from flask import session
+from flask.ext.login import login_user
+from functools import wraps
+import unittest
+import os
+import tempfile
+
+
+def ignore(*exceptions):
+    """ Use as decorator when you want to ignore certain exceptions.
+
+    Example:
+
+        @ignore(OSError)
+        def sketchy():
+            os.remove('does_this_exists.txt')
+    """
+
+    def wrapper(func):
+
+        @wraps(func)
+        def inner(*args, **kwargs):
+            try:
+                func(*args, **kwargs)
+            except exceptions:
+                pass
+        return inner
+    return wrapper
+
+
+class HTTPTestHelper(unittest.TestCase):
+
+    def _assert_response_factory(self, status_code):
+        def _assert_response(response):
+            self.assertEqual(response.status_code, status_code)
+        return _assert_response
+
+
+    def __init__(self, *args, **kwargs):
+        super(HTTPTestHelper, self).__init__(*args, **kwargs)
+        for i in (200, 201, 204, 301, 302, 400, 401, 403, 404):
+            setattr(self, 'assert%d' % i, self._assert_response_factory(i))
+
+
+class UserTestCase(HTTPTestHelper):
+
+    def create_session(self, user):
+        with self.app.test_request_context():
+            db.session.add(user)
+            db.session.commit()
+
+            # Login the user and save the session
+            login_user(user)
+            session_copy = {k: v for k, v in session.items()}
+
+        # Re-create the session with a new test client
+        with self.app.test_client() as c:
+            with c.session_transaction() as sess:
+                for k, v in session_copy.items():
+                    sess[k] = v
+            return c
+
+
+    def preSetUp(self):
+        self.test_db = tempfile.NamedTemporaryFile(delete=False)
+        self.test_db.close()
+        self.app = create_app(SQLALCHEMY_DATABASE_URI='sqlite:///' + self.test_db.name,
+            WTF_CSRF_ENABLED=False, SECRET_KEY='bogus')
+        self.admin_user = self.create_session(User(first_name='Bob', last_name='Admin', is_admin=True))
+        self.anon_user = self.app.test_client()
+        self.auth_user = self.create_session(User(first_name='Alice', last_name='User'))
+
+
+    def __call__(self, *args, **kwargs):
+        self.preSetUp()
+        super(UserTestCase, self).__call__(*args, **kwargs)
+        self.postTearDown()
+
+
+    @ignore(OSError)
+    def postTearDown(self):
+        os.remove(self.test_db.name)
+
--- blag/tests/test_blog.py
+++ blag/tests/test_blog.py
@@ -1,61 +1,13 @@
-from .. import create_app, db
+from . import UserTestCase
+from .. import db
 from ..models import BlogPost
-from ..auth.models import User
 
-from flask import session
-from flask.ext.login import login_user
 import json
-import tempfile
-import unittest
 
 
-class UserTestCase(unittest.TestCase):
-
-    def create_session(self, user):
-        with self.app.test_request_context():
-            db.session.add(user)
-            db.session.commit()
-
-            # Login the user and save the session
-            login_user(user)
-            session_copy = {k: v for k, v in session.items()}
-
-        # Re-create the session with a new test client
-        with self.app.test_client() as c:
-            with c.session_transaction() as sess:
-                for k, v in session_copy.items():
-                    sess[k] = v
-            return c
-
-
-    def preSetUp(self):
-        self.test_db = tempfile.NamedTemporaryFile(delete=False)
-        self.test_db.close()
-        self.app = create_app(SQLALCHEMY_DATABASE_URI='sqlite:///' + self.test_db.name,
-            WTF_CSRF_ENABLED=False, SECRET_KEY='bogus')
-        self.admin_user = self.create_session(User(first_name='Bob', last_name='Admin', is_admin=True))
-        self.anon_user = self.app.test_client()
-        self.auth_user = self.create_session(User(first_name='Alice', last_name='User'))
-
-
-    def __call__(self, *args, **kwargs):
-        self.preSetUp()
-        super(UserTestCase, self).__call__(*args, **kwargs)
-        self.postTearDown()
-
-
-    def postTearDown(self):
-        pass
-
-
-class BlogTest(unittest.TestCase):
+class BlogTest(UserTestCase):
 
     def setUp(self):
-        self.test_db = tempfile.NamedTemporaryFile(delete=False)
-        self.test_db.close()
-        self.app = create_app(SQLALCHEMY_DATABASE_URI='sqlite:///' + self.test_db.name,
-            WTF_CSRF_ENABLED=False, SECRET_KEY='bogus')
-        self.client = self.app.test_client()
         with self.app.app_context():
             db.session.add(BlogPost(title='Test article', rendered_content='Snip', raw_content='Snip'))
             db.session.commit()
@@ -63,15 +15,15 @@ class BlogTest(unittest.TestCase):
 
 
     def test_main_page(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        response = self.anon_user.get('/')
+        self.assert200(response)
         self.assertTrue('Test article' in response.data)
         self.assertTrue('Snip' in response.data)
 
 
     def test_post_details(self):
-        response = self.client.get('/blag/%d' % self.post_id)
-        self.assertEqual(response.status_code, 200)
+        response = self.anon_user.get('/blag/%d' % self.post_id)
+        self.assert200(response)
         self.assertTrue('Test article' in response.data)
         self.assertTrue('Snip' in response.data)
 
@@ -84,14 +36,12 @@ class WritePostTest(UserTestCase):
             'raw_content': json.dumps({'data': [{'type': 'text', 'data': {'text': 'Something'}}]}),
         }
         response = self.admin_user.post('/blog', data=contents)
-        self.assertEqual(response.status_code, 302)
+        self.assert302(response)
         with self.app.app_context():
             blogposts = BlogPost.query.all()
             self.assertEqual(len(blogposts), 1)
 
 
     def test_write_restricted(self):
-        response = self.anon_user.post('/blog')
-        self.assertEqual(response.status_code, 401)
-        response = self.auth_user.post('/blog')
-        self.assertEqual(response.status_code, 403)
+        self.assert401(self.anon_user.post('/blog'))
+        self.assert403(self.auth_user.post('/blog'))
