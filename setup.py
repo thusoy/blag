0251c026fd43cb17cafc43e5728faf8b0b68e830
Tarjei Husøy <git@thusoy.com>
2016-06-11 13:08:21 -0700

    Ensure migrations are included in build

--- setup.py
+++ setup.py
@@ -2,7 +2,26 @@
 # coding: utf-8
 
 from setuptools import setup, find_packages
-from os import path
+import os
+
+
+def package_files(directory, relative_to):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            full_path = os.path.join(path, filename)
+            final_path = os.path.relpath(full_path, relative_to)
+            paths.append(final_path)
+    return paths
+
+migration_files = package_files(os.path.join('blag', 'migrations'), os.path.abspath('blag'))
+package_data = [
+    os.path.join('templates', '*.html'),
+    os.path.join('templates', '*', '*.html'),
+    os.path.join('server-assets', '*'),
+]
+
+package_data.extend(migration_files)
 
 setup(
     name='thusoy-blag',
@@ -13,11 +32,7 @@ setup(
     description='Somewhere to ramble',
     packages=find_packages(),
     package_data={
-        '': [
-            path.join('templates', '*.html'),
-            path.join('templates', '*', '*.html'),
-            path.join('server-assets', '*'),
-        ],
+        '': package_data
     },
     zip_safe=False,
     entry_points={
